2266ff4a6683264ac40c09b6e66031b45e8b904d
KJ <kaijun_95@hotmail.com>
2017-11-10 23:17:11 +0800

    Deleted old files Added new files New manifest file content Created new logo Basic background.js file content

--- Youtube Audio/background.js
+++ Youtube Audio/background.js
@@ -1,62 +1,29 @@
-var screenshot = {
-    content: document.createElement("canvas"),
-    data : '',
 
-    init : function() {
-        this.initEvents();
-    },
+function enableExtension() {
+    chrome.browserAction.setIcon({
+        path : {
+            32 : "icons/logo_32.png",
+            48 : "icons/logo_48.png",
+        }
+    });
+    chrome.tabs.onUpdated.addListener(sendMessage);
+    chrome.webRequest.onBeforeRequest.addListener(
+        processRequest,
+        {urls: ["<all_urls>"]},
+        ["blocking"]
+    );
+}
 
-    saveScreenshot : function() {
-        var image = new Image();
-        image.onload = function() {
-            var canvas = screenshot.content;
-            canvas.width = image.width;
-            canvas.height = image.height;
-            var context = canvas.getContext("2d");
-            context.drawImage(image, 0, 0);
+function disableExtension() {
+    chrome.browserAction.setIcon({
+        path : {
+            32 : "icons/logo_32.png",
+            48 : "icons/logo_48.png",
+        }
+    });
+    chrome.tabs.onUpdated.removeListener(sendMessage);
+    chrome.webRequest.onBeforeRequest.removeListener(processRequest);
+    tabIds.clear();
 
-            //save image
-            var link = document.createElement('a');
-            link.download = "image.png";
-            link.href = screenshot.content.toDataURL();
-            link.click();
-            screenshot.data = '';
-        };
-        image.src = screenshot.data;
-    },
-
-    initEvents : function() {
-        firefox.browserAction.onClicked.addListener(function(tab){
-            firefox.tabs.captureVisibleTab(null, {
-                format: "png",
-                quality: 100
-            }, function(data){
-                screenshot.data = data;
-
-                //send an alert message to webpage
-                firefox.tabs.query({
-                    
-                    active : true,
-                    currentWindow : true
-                    
-                }, function(tabs) {
-                    firefox.tabs.sendMessage(tabs[0].id, {ready : "ready"}, function(response) {
-                
-                        if (response.download === "download") {
-                            screenshot.saveScreenshot();
-                        }
-                        else {
-                            screenshot.data = '';
-                        }
-                    });
-                });
-            });
-        });
-    }
-};
-
-screenshot.init();
-
-
-
+}
 
