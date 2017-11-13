be82f61da191b77aa9beea308394b8aeea2e8dc2
KJ <kaijun_95@hotmail.com>
2017-11-14 00:59:34 +0800

    Trying to implement the function

--- Youtube Audio/content.js
+++ Youtube Audio/content.js
@@ -10,3 +10,13 @@ function makeSetAudioURL(videoElement, url) {
     setAudioURL();
     return setAudioURL;
 }
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        var url = request.url;
+        var videoElements = document.getElementsByTagName('video');
+        var videoElement = videoElements[0];
+        if (typeof videoElement == "undefined") {
+            console.log("Audio Only Youtube - Video element undefined in this frame!");
+            return;
+        }
