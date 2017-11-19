02892945009cea63ab6b975b73e731e689b92636
KJ <kaijun_95@hotmail.com>
2017-11-20 04:52:12 +0800

    Added some function Added new icons

--- Youtube Audio/background.js
+++ Youtube Audio/background.js
@@ -1,4 +1,58 @@
+var map = (function() {
+    var _maxLength = 200;
 
+    var _map = Object.create(null);
+    var _keys = [];
+
+    function map() {
+        this.len = function() {
+            return _keys.length;
+        };
+
+        this.setMaxLength = function(len) {
+            _maxLength = len;
+        };
+
+        this.getMaxLength = function() {
+            return _maxLength;
+        };
+
+        this.insert = function(key, value) {
+            if (this.len.apply() == this.getMaxLength.apply() &&
+                typeof _map[key] == "undefined") {
+                var id = _keys.shift();
+                delete _map[id];
+            }
+
+            _map[key] = value;
+            if (!this.contains(key)) {
+                _keys.push(key);
+            }
+        };
+
+        this.value = function(key) {
+            return _map[key];
+        };
+
+        this.contains = function(key) {
+            return typeof _map[key] != "undefined";
+        };
+
+        this.remove = function(key) {
+            if (this.contains(key)) {
+                delete _map[key];
+            }
+        };
+
+        this.clear = function() {
+            _map = Object.create(null);
+            _keys = [];
+        }
+    }
+
+    return map;
+
+})();
 function enableExtension() {
     chrome.browserAction.setIcon({
         path : {
